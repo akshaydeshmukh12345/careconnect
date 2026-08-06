ab01c5e9ea772976a2025e6bd29958757b523477
unknown <akshaydeshmukh12345@gmail.com>
2026-08-07 03:02:51 +0530

    feat: install React Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div>
-      <h1>CareConnect</h1>
-    </div>
+    <main className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        CareConnect
+      </h1>
+    </main>
   );
 }
 
